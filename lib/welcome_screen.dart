7f039036e66add2524c27cb19481517bf86d47fa
UP2005572 <91529143+UP2005572@users.noreply.github.com>
2023-03-24 10:29:16 +0000

    add welcome screen

---
+++ lib/welcome_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Welcome to Bitmapp'd", style: TextStyle(fontSize: 50),),
+            SizedBox(height: 50),
+            SizedBox(
+              height: 50,
+              width: 100,
+              child: ElevatedButton(
+                onPressed: () {  },
+                child: Center(child: Text("Start", style: TextStyle(color: Colors.white, fontSize: 20),), )
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
