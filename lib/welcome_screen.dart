3364f99d3d53845acd1a960dd7ceaf759a7007b1
UP2005572 <91529143+UP2005572@users.noreply.github.com>
2023-03-24 16:16:57 +0000

    changeable image sizing

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -2,7 +2,11 @@ import 'package:bitmappd/workspace.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({Key? key}) : super(key: key);
+  TextEditingController _heightController = TextEditingController();
+  TextEditingController _widthController = TextEditingController();
+  TextEditingController _colourDepthController = TextEditingController();
+
+  WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,20 +15,69 @@ class WelcomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Welcome to Bitmapp'd", style: TextStyle(fontSize: 50),),
-            SizedBox(height: 50),
+            Text(
+              "Welcome to Bitmapp'd",
+              style: TextStyle(fontSize: 50),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 400,
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: _heightController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Image Height (px)")),
+                  )),
+                  SizedBox(width: 10),
+                  Expanded(
+                      child: TextField(
+                    controller: _widthController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Image Width (px)")),
+                  ))
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+                width: 195,
+                child: TextField(
+                  controller: _colourDepthController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text('Colour Depth (bits)')),
+                )),
+            SizedBox(height: 20),
             SizedBox(
-              height: 50,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {   Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Workspace())); },
-                child: Center(child: Text("Start", style: TextStyle(color: Colors.white, fontSize: 20),), )
-              )
-            )
+                height: 50,
+                width: 100,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Workspace(
+                                    imageHeight: int.parse(
+                                        _heightController.text.trim()),
+                                    imageWidth:
+                                        int.parse(_widthController.text.trim()),
+                                    colourDepth: int.parse(
+                                        _colourDepthController.text.trim()),
+                                  )));
+                    },
+                    child: Center(
+                      child: Text(
+                        "Start",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    )))
           ],
         ),
       ),
     );
   }
 }
-
--- lib/workspace.dart
+++ lib/workspace.dart
@@ -3,7 +3,11 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 
 class Workspace extends StatefulWidget {
-  const Workspace({Key? key}) : super(key: key);
+  final int imageHeight;
+  final int imageWidth;
+  final int colourDepth;
+
+  const Workspace({Key? key, required this.imageHeight, required this.imageWidth, required this.colourDepth}) : super(key: key);
 
   @override
   State<Workspace> createState() => _WorkspaceState();
@@ -13,25 +17,66 @@ class _WorkspaceState extends State<Workspace> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  VisualGrid(height: 10, width: 10,),
-                ],
-              ),
-            ],
+      body: Container(
+        color: Colors.lightBlue,
+        child: Center(
+          child: Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                          boxShadow: panelShadow,
+                            borderRadius: BorderRadius.circular(30)),
+
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              Text('Input (Bits)', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              BitGrid(height: widget.imageHeight, width: widget.imageWidth),
+                            ],
+                          ),
+                        )),
+                    SizedBox(width: 100),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                        boxShadow: panelShadow,
+                          borderRadius: BorderRadius.circular(30)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Text('Output (Image)', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            VisualGrid(
+                              height: widget.imageHeight,
+                              width: widget.imageWidth,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
 
 class VisualCell extends StatefulWidget {
   const VisualCell({Key? key}) : super(key: key);
@@ -44,9 +89,12 @@ class _VisualCellState extends State<VisualCell> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 30,
-      width: 30,
-      decoration: BoxDecoration(border: Border.all(color: Colors.black,)),
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+          border: Border.all(
+        color: Colors.black,
+      )),
     );
   }
 }
@@ -64,15 +112,68 @@ class VisualGrid extends StatefulWidget {
 class _VisualGridState extends State<VisualGrid> {
   @override
   Widget build(BuildContext context) {
-    return Column( children: List.generate(widget.height, (index) {
-      return Row(children: List.generate(widget.width, (index) {
-        return VisualCell();
-      }),);
-    })
+    return Column(
+        children: List.generate(widget.height, (index) {
+      return Row(
+        children: List.generate(widget.width, (index) {
+          return VisualCell();
+        }),
+      );
+    }));
+  }
+}
 
+class BitCell extends StatefulWidget {
+  const BitCell({Key? key}) : super(key: key);
+
+  @override
+  State<BitCell> createState() => _BitCellState();
+}
+
+class _BitCellState extends State<BitCell> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 50,
+      height: 50,
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.zero),
+        ),
+      ),
     );
   }
 }
 
+class BitGrid extends StatefulWidget {
+  final int height;
+  final int width;
 
+  const BitGrid({Key? key, required this.height, required this.width})
+      : super(key: key);
 
+  @override
+  State<BitGrid> createState() => _BitGridState();
+}
+
+class _BitGridState extends State<BitGrid> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: List.generate(widget.height, (index) {
+      return Row(
+        children: List.generate(widget.width, (index) {
+          return BitCell();
+        }),
+      );
+    }));
+  }
+}
+
+final List<BoxShadow> panelShadow = [
+  BoxShadow(
+    color: Colors.black.withOpacity(0.2),
+    blurRadius: 10,
+    offset: const Offset(0, 3),
+  ),
+];
